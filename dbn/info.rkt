1a51270faffb834f392fd1e67916fcd4f2c8ce2d
Chris GauthierDickey <2659858+chrisgd@users.noreply.github.com>
2019-01-24 20:54:54 -0400

    Renaming manual.scrbl to dbn-manual.scrbl

--- dbn/info.rkt
+++ dbn/info.rkt
@@ -6,9 +6,9 @@
 ; build dependencies, we need scribble, docs and rackunit for testing
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 ; location of documentation
-(define scribblings '(("scribblings/manual.scrbl" (multi-page) (language))))
+(define scribblings '(("scribblings/dbn-manual.scrbl" (multi-page) (language))))
 ; things to skip tests
-(define test-omit-paths '("scribblings/manual.scrbl")) 
+(define test-omit-paths '("scribblings/dbn-manual.scrbl")) 
 ; first release of design by numbers
 (define pkg-desc "Implementation of DBN (Design by Numbers) language")
 ; version number
